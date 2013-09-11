b6cafbc12ddb45921cae1952a9c7579e043b96d8
Daniel P. Seemuth <dps-github-datasettools@seemuth.com>
2013-09-11 16:20:12 -0500

    Add missing semicolons

--- example_rand_col.m
+++ example_rand_col.m
@@ -1,12 +1,12 @@
 % Create problem source data.
 numrows = 16;
-data = []
+data = [];
 
 % Column 1: uniform distribution on [-4, 4], rounded to the nearest 0.01
 data(:,1) = round(rand_col(numrows, -4, 4) * 100) / 100;
 
 % Column 2: multiples of 0.25 between 8 and 12
-data(:,2) = rand_col(numrows, 8, 12, 0.25)
+data(:,2) = rand_col(numrows, 8, 12, 0.25);
 
 % Compute answer column.
 data(:,3) = data(:,1) + data(:,2);
