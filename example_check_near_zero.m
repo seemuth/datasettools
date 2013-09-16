577e9f871dc04bc44d2dd02e9e9f4941a4fbd127
Daniel P. Seemuth <dps-github-moodle@seemuth.com>
2013-09-16 16:14:01 -0500

    Add third column of data that rarely contains values near zero

--- example_check_near_zero.m
+++ example_check_near_zero.m
@@ -10,6 +10,9 @@ data(:,1) = round(rand_col(numrows, -2, 2) * 100) / 100;
 % Column 2: multiples of 0.125 between -4 and 4
 data(:,2) = rand_col(numrows, -4, 4, 0.125);
 
+% Column 3: uniform distribution on [0, 10].
+data(:,3) = rand_col(numrows, 0, 10);
+
 % Force certain values to be near zero for demonstration purposes.
 data(8:12,1) = linspace(-0.04, 0.04, 5);
 data(8:12,2) = linspace(-0.25, 0.25, 5);
@@ -22,7 +25,7 @@ fprintf('\nNumber of values near zero: %d\n', num_near_zero);
 
 fprintf('\n%s\n', sep);
 
-for col = 1:2
+for col = 1:3
     fprintf('IN COLUMN %d:\n', col);
 
     % Check for values near zero in only specified column(s).
